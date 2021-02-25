1161f35f676f6fe4bf370679e08240ca86ee4425
MinhQuan Tran <minhquan.tran@wsu.edu>
2021-02-25 13:46:06 -0800

    Updated web app to use new REST API Also did some prettify along the way

--- frontend/src/components/DefaultDisplay.js
+++ frontend/src/components/DefaultDisplay.js
@@ -1,43 +1,19 @@
-import React, { useState } from 'react';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
-import Divider from '@material-ui/core/Divider';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Chart from './Chart';
-import PatientTable from './Patient';
-import Title from './Title';
-import { gql, useQuery } from '@apollo/client';
-import {client} from '../graphql-logic/endpoint'
-import {patientData} from '../graphql-logic/queries'
-
-// Test fetch
-client.query({query: patientData}).then(response => {
-    console.log(response.data)
-})
-const patient_query = gql`
-  {allSignals(signalRecordName_RecordName:100, time: "0,30"){
-  pageInfo{
-    startCursor
-    endCursor
-    hasNextPage
-    hasPreviousPage
-  }
-  edges{
-    node{
-      id
-      time
-      mlii
-      v5
-    }
-  }
-}}
-`;
+import React, { useState } from "react";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import Divider from "@material-ui/core/Divider";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Chart from "./Chart";
+import PatientTable from "./Patient";
+import Title from "./Title";
+import { useQuery } from "@apollo/client";
+import { signalQuery } from "../graphql-logic/queries";
 
 let beats = [0.214, 1.028, 1.844, 2.631, 3.419, 4.206, 5.025];
-  let annotations = ['N', 'N', 'N', 'N', 'N', 'N', 'N'];
+let annotations = ["N", "N", "N", "N", "N", "N", "N"];
 // Generate pairs of timestamps and readings
 function createData(time, amount) {
   return { time, amount };
@@ -46,28 +22,28 @@ function createData(time, amount) {
 const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
+    display: "flex",
   },
   title: {
     flexGrow: 1,
   },
   drawerPaper: {
-    position: 'relative',
-    whiteSpace: 'nowrap',
+    position: "relative",
+    whiteSpace: "nowrap",
     width: drawerWidth,
-    transition: theme.transitions.create('width', {
+    transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
   },
   drawerPaperClose: {
-    overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
+    overflowX: "hidden",
+    transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
     width: theme.spacing(7),
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up("sm")]: {
       width: theme.spacing(9),
     },
   },
@@ -77,9 +53,9 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(2),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column",
   },
   fixedHeight: {
     height: 240,
@@ -87,60 +63,66 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function updateGraph(data) {
-  let signals = data.allSignals.edges;
+  let signals = data.patient.results;
   let MLIIdatapoints = [];
   let V5datapoints = [];
   let i = 0;
 
-  for(i; i < 2000; i++){
-    MLIIdatapoints.push(createData(signals[i].node.time, signals[i].node.mlii));
-    V5datapoints.push(createData(signals[i].node.time, signals[i].node.v5));
+  for (i; i < 2000; i++) {
+    MLIIdatapoints.push(createData(signals[i].time, signals[i].mlii));
+    V5datapoints.push(createData(signals[i].time, signals[i].v5));
   }
-  return {ml2: MLIIdatapoints, v5: V5datapoints}
+  return { ml2: MLIIdatapoints, v5: V5datapoints };
 }
 
 export default function Sample() {
-	const classes = useStyles();
+  const classes = useStyles();
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
-  const { loading, error, data } = useQuery(patient_query);
-  const [dataset, setDataset] = useState(0)
+  const { loading, error, data } = useQuery(signalQuery);
+  const [dataset, setDataset] = useState(0);
 
-  if (loading) return 'Loading...';
+  if (loading) return "Loading...";
   if (error) return `Error! ${error.message}`;
-  let signals = data.allSignals.edges;
+  let signals = data.patient.results;
 
   let MLIIdatapoints = [];
   let V5datapoints = [];
   let i = 0;
 
-  for(i; i < 2000; i++){
-    MLIIdatapoints.push(createData(signals[i].node.time, signals[i].node.mlii));
-    V5datapoints.push(createData(signals[i].node.time, signals[i].node.v5));
+  for (i; i < 2000; i++) {
+    MLIIdatapoints.push(createData(signals[i].time, signals[i].mlii));
+    V5datapoints.push(createData(signals[i].time, signals[i].v5));
   }
-  
 
-	return (
-		<Container maxWidth="lg" className={classes.container}>
-          <Grid container spacing={3}>
-            {/* Chart */}
-            <Grid item xs={12} md={10} lg={12}>
-              <Paper className={fixedHeightPaper}>
-                <Title>ML II</Title>
-                <Chart key={1} data={MLIIdatapoints} beats={beats} annotations={annotations}/>
-              </Paper>
-              <Divider />
-              <Paper className={fixedHeightPaper}>
-                <Title>V1</Title>
-                <Chart key={2} data={V5datapoints} beats={beats} annotations={annotations}/>
-              </Paper>
-            </Grid>
-          </Grid>
-          <PatientTable>
-          </PatientTable>
+  return (
+    <Container maxWidth="lg" className={classes.container}>
+      <Grid container spacing={3}>
+        {/* Chart */}
+        <Grid item xs={12} md={10} lg={12}>
+          <Paper className={fixedHeightPaper}>
+            <Title>ML II</Title>
+            <Chart
+              key={1}
+              data={MLIIdatapoints}
+              beats={beats}
+              annotations={annotations}
+            />
+          </Paper>
+          <Divider />
+          <Paper className={fixedHeightPaper}>
+            <Title>V1</Title>
+            <Chart
+              key={2}
+              data={V5datapoints}
+              beats={beats}
+              annotations={annotations}
+            />
+          </Paper>
+        </Grid>
+      </Grid>
+      <PatientTable></PatientTable>
 
-          <Box pt={4}>
-            
-          </Box>
-        </Container>
-        );
+      <Box pt={4}></Box>
+    </Container>
+  );
 }
--- frontend/src/graphql-logic/queries.js
+++ frontend/src/graphql-logic/queries.js
@@ -2,12 +2,13 @@ import { gql } from "@apollo/client";
 /* For GraphQL */
 // Note: When specifying path, the path is concatenated with the rest link.
 //       It is sensitive to characters such as '/'
-export const patientData = gql`
+export const signalQuery = gql`
   query getPatient {
     patient
       @rest(
         type: "Patient"
-        path: "signals/?format=json&signal_record_name=4&timeRange=0,30"
+        path: "/?format=json&signal_record_name=4&timeRange=0,30"
+        endpoint: "signal"
       ) {
       count
       next
