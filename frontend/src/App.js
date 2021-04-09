f2ab3316eb72fb05d1e1ba1b454567541f3ca0b1
MinhQuan Tran <minhquan.tran@wsu.edu>
2021-04-09 01:09:12 -0700

    Allow option to set default to dark theme Currently use light to match graph scheme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,34 @@
 import "./App.css";
+import React from 'react';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Dashboard from "./components/Dashboard";
 
 function App() {
+  // Theme of app can be specified, can set default to dark.
+  // Uncomment line 13 to switch defaults 
+  let defaultThemeMode = useMediaQuery('(prefers-color-scheme: light)');
+  // defaultThemeMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const prefersDarkMode = defaultThemeMode
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
   return (
+
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
     <div className="App">
       <Dashboard />
     </div>
+    </ThemeProvider>
   );
 }
 
