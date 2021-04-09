e50a2e959cf8f295ef8fd4cab45bbca58c088be7
MinhQuan Tran <minhquan.tran@wsu.edu>
2021-04-09 12:49:25 -0700

    Prettify even more

--- backend/patientdb/serializers.py
+++ backend/patientdb/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers 
+from rest_framework import serializers
 from patientdb.models import Patient, Signals
 from rest_framework.parsers import MultiPartParser, FormParser
 
@@ -7,16 +7,14 @@ class UploadSerializer(serializers.Serializer):
     parser_classes = (MultiPartParser, FormParser,)
     #file = serializers.FileField(use_url=False)
     file = serializers.ListField(
-                child=serializers.FileField( max_length=100000,
-                                             allow_empty_file=False,
-                                             use_url=False )
-            )
-
-    
+        child=serializers.FileField(max_length=100000,
+                                    allow_empty_file=False,
+                                    use_url=False)
+    )
 
 
 class PatientSerializer(serializers.ModelSerializer):
- 
+
     class Meta:
         model = Patient
         fields = ("record_name", "n_sig", "fs", "counter_freq", "base_counter", "sig_len", "base_time", "base_date",
@@ -24,10 +22,9 @@ class PatientSerializer(serializers.ModelSerializer):
                   "skew", "byte_offset", "adc_gain", "baseline", "units", "adc_res", "adc_zero", "init_value",
                   "checksum", "block_size", "has_annotations")
 
+
 class SignalsSerializer(serializers.ModelSerializer):
-    
 
     class Meta:
         model = Signals
-        fields =('signal_record_name', 'time', 'mlii', 'v5', 'annotation')
-    
+        fields = ('signal_record_name', 'time', 'mlii', 'v5', 'annotation')
--- backend/patientdb/urls.py
+++ backend/patientdb/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import url 
-from patientdb import views 
+from django.urls import path
+from django.conf.urls import url
+from patientdb import views
 from rest_framework.routers import DefaultRouter
 from .views import UploadedFileViewSet
 from rest_framework import generics
@@ -10,9 +11,9 @@ router.register(r'patients', views.PatientViewSet)
 #router.register(r'uploadPatient',  views.UploadedFileViewSet, basename='uploadPatient')
 
 # Currently testing integrating ML into app, might be used in actual implementation
-from django.urls import path
 urlpatterns = [
-    path('predict_mlii_signals/', views.Predict_Signals.as_view(), name='api_predict_mlii_signals'),
+    path('predict_mlii_signals/', views.Predict_Signals.as_view(),
+         name='api_predict_mlii_signals'),
     path('uploadPatient/', views.UploadedFileViewSet.as_view(), name='uploadPatient')
 ]
 
