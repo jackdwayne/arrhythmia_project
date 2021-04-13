6f3594e3a159fda725f90117b86456eb20c4881b
Ryker Gilbert <ryker.gilbert@wsu.edu>
2021-04-13 11:39:12 -0700

    Prediction annotations added to chart

--- backend/mainapp/settings.py
+++ backend/mainapp/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'patient_db',
         'USER': 'postgres',
-        'PASSWORD': 'heart420',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
      }
